53601e9f9c05b9a3a4c9449178bfd2dbfcc2476b
Prez <prezhome@Prezs-Mac-mini.local>
2026-04-01 17:39:30 -0400

    Standardize static export for Google Sites embedding

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // standard config
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 module.exports = nextConfig
